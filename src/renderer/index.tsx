1ded9fb914eca3b78a2f70be836c8bb398af86d0
ktyoung <ktyoung0507@gmail.com>
2023-08-03 22:19:31 +0900

    fix some

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -9,18 +9,18 @@ const root = createRoot(container);
 root.render(<App />);
 
 // calling IPC exposed from preload script
-window.electron.ipcRenderer.once('ipc', (arg) => {
-  console.log(arg);
-});
+// window.electron.ipcRenderer.once('ipc', (arg) => {
+//   console.log(arg);
+// });
 
-window.electron.ipcRenderer.sendMessage('ipc', ['ping']);
+// window.electron.ipcRenderer.sendMessage('ipc', ['ping']);
 
-window.electron.ipcRenderer.on('load-data-failured', (message) => {
-  alertCustom({ message: message as string });
-});
+// window.electron.ipcRenderer.on('load-data-failured', (message) => {
+//   alertCustom({ message: message as string });
+// });
 
-window.electron.ipcRenderer.on('save-file-failured', (message) => {
-  alertCustom({ message: message as string });
-});
+// window.electron.ipcRenderer.on('save-file-failured', (message) => {
+//   alertCustom({ message: message as string });
+// });
 
-console.info('Log from the renderer process');
+// console.info('Log from the renderer process');
